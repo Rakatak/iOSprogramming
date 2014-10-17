1f7c37d13aff11d91a37912163da940872d4596a
s797459 s797459 <s797459@swe36m.swedom.virt>
2014-10-17 13:54:16 +0200

    second task

---
+++ aufgabe2/aufgabe2/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  aufgabe2
+//
+//  Created by s797459 s797459 on 17.10.14.
+//  Copyright (c) 2014 s797459. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var digitsResult: UILabel!
+    
+    @IBOutlet weak var digitsInput: UILabel!
+    
+    var currentDigit = ""
+    
+    var previousDigit = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        digitsInput.text = ""
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func digitButton(sender: AnyObject) {
+        println("Digit")
+        currentDigit = digitsInput.text!
+        var digit = sender.currentTitle
+        var finalDigit = currentDigit.stringByAppendingString(digit!!)
+        digitsInput.text = finalDigit
+    }
+    
+    @IBAction func addButton(sender: AnyObject) {
+        
+        if (previousDigit.isEmpty || digitsInput.text!.isEmpty){
+           println("enter additional number first")
+        } else {
+            currentDigit = digitsInput.text!
+            var currentDig = String(currentDigit).toInt()
+            var previousDig =  String(previousDigit).toInt()
+            var finalRes = currentDig! + previousDig!
+            previousDigit = String(finalRes)
+            digitsInput.text = ""
+            currentDigit = ""
+            digitsResult.text = previousDigit
+
+        }
+
+    }
+
+    @IBAction func subtract(sender: AnyObject) {
+    }
+    
+    @IBAction func multiplyButton(sender: AnyObject) {
+    }
+    
+    @IBAction func divideButton(sender: AnyObject) {
+    }
+    
+    @IBAction func enterButton(sender: AnyObject) {
+        if (previousDigit.isEmpty){
+            previousDigit = currentDigit
+            currentDigit = ""
+            digitsInput.text = ""
+
+        } else {
+            digitsResult.text = previousDigit
+            println("not available")
+        }
+        
+        
+    }
+    
+}
+
---
+++ aufgabe2/aufgabe2Tests/aufgabe2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  aufgabe2Tests.swift
+//  aufgabe2Tests
+//
+//  Created by s797459 s797459 on 17.10.14.
+//  Copyright (c) 2014 s797459. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class aufgabe2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
