fb1a8140edaed543557e37c8ef6d70e10c3ca0c6
Rakatak <robin-steller@web.de>
2014-11-11 19:59:57 +0100

    finished second task

--- Aufgabe4/Teilaufgabe2.playground/section-1.swift
+++ Aufgabe4/Teilaufgabe2.playground/section-1.swift
@@ -1,51 +1,56 @@
-// Playground - noun: a place where people can play
+ // Playground - noun: a place where people can play
 
 import Foundation
 
-enum VehicleClass {
-    case PKW(serial: String)
-    case LKW(serial: String, axis: Int, weight: Int)
+class Employee {
+    var firstName: String
+    var lastName: String
+    var employeeID: Int
+    var employDate: Int
+    var workingHours: Int
+    lazy var salary = Double()
+    class var hourlyPay: Double {
+        return 7.99
+    }
+    
+    init(firstName: String, lastName: String, employeeID: Int, employDate: Int, workingHours: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.employeeID = employeeID
+        self.employDate = employDate
+        self.workingHours = workingHours
+        self.salary = Double(workingHours) * myHourlyPay
+    }
+    
+    var years: Int {
+        get {
+            return 2014 - employDate
+        }
+    }
+    
+    var myHourlyPay: Double {
+        get {
+            return Employee.hourlyPay + Double(years)*0.75
+        }
+    }
+    
+    func showSalary(){
+        println("\(salary)€")
+    }
 }
 
 println("Hello Test")
 
 
 
-var honda = VehicleClass.PKW(serial: "ISAMEN76UHW6")
-var toyota = VehicleClass.PKW(serial: "IWBABA76UHW6")
-var daewoo = VehicleClass.PKW(serial: "USIAHF")
+var brianFury = Employee(firstName: "Brian", lastName: "Fury", employeeID: 77, employDate: 1999, workingHours: 20)
+var leiWulong = Employee(firstName: "Lei", lastName: "Wulong", employeeID: 231, employDate: 1976, workingHours: 40)
+var uweBoll = Employee(firstName: "Uwe", lastName: "Boll", employeeID: 1, employDate: 2002, workingHours: 8)
 
+println(leiWulong.salary)
+println(uweBoll.salary)
+println(brianFury.salary)
 
-var dodge = VehicleClass.LKW(serial: "ISAMEN76UHW6", axis: 4, weight: 70)
-var ford = VehicleClass.LKW(serial: "8SUEB344", axis: 1, weight: 10)
-var volvo = VehicleClass.LKW(serial: "VCBE63HJ90SK", axis: 4, weight: 8)
+brianFury.showSalary()
 
-
-func plausibilityTest(vehicle: VehicleClass) -> String {
-    
-    switch vehicle {
-    case var .PKW(serial):
-        if (serial.serial.lowercaseString.rangeOfString("wbaba") != nil || countElements(serial.serial) != 12) {
-            return "That is not a valid car!"
-        }
-        return "It's a real car!"
-        
-    case var .LKW(serial, axis, weight):
-        if (countElements(serial) != 12) {
-            return "That is not a valid truck, something's wrong with the serial!"
-        }
-        if (weight/axis > 2){
-            return "Trucks's maximum weight exceeded!"
-        }
-        return "It's a real truck!"
-        
-    }
-}
-
-plausibilityTest(honda)
-plausibilityTest(toyota)
-plausibilityTest(daewoo)
-
-plausibilityTest(volvo)
-plausibilityTest(ford)
-plausibilityTest(dodge)
+println(leiWulong.myHourlyPay)
